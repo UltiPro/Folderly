becf23a79d1adf6ac434b0069d988078f15bf250
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-11-16 17:02:12 +0100

    DB Models and Init Requirements

---
+++ Folderly/Backend/models/__init__.py
@@ -0,0 +1,3 @@
+from models.user import UserModel
+from models.folder import FolderModel
+from models.userfolder import UserFolderModel
---
+++ Folderly/Backend/models/user.py
@@ -0,0 +1,13 @@
+from Database import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    ban = db.Column(db.Boolean, default=False, nullable=False)
+    folders = db.relationship(
+        "FolderModel", back_populates="users", secondary="usersfolders"
+    )
