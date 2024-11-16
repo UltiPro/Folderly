2c9107741c6278520897f88f4a4a3a611610a937
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-11-16 22:18:52 +0100

    Blocklist model

--- Folderly/Backend/models/__init__.py
+++ Folderly/Backend/models/__init__.py
@@ -1,3 +1,4 @@
 from models.user import UserModel
 from models.folder import FolderModel
 from models.userfolder import UserFolderModel
+from models.blocklist import BlocklistModel
---
+++ Folderly/Backend/models/blocklist.py
@@ -0,0 +1,7 @@
+from Database import db
+
+
+class BlocklistModel(db.Model):
+    __tablename__ = "blocklist"
+
+    token = db.Column(db.String, primary_key=True)
