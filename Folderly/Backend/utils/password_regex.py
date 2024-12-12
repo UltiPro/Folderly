2fad0e39729c5a2bef6334ccaddff3f8f33eb0d9
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-12-12 22:42:17 +0100

    New settings

--- Folderly/Backend/utils/password_regex.py
+++
@@ -1,2 +0,0 @@
-password_regex = r"(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[.~!@#$%^&*()+=[\]\\;:'\"/,|{}<>?])[a-zA-Z0-9.~!@#$%^&*()+=[\]\\;:'\"/,|{}<>?]{8,40}"
-password_regex_desc = "Password must be between 8 and 40 characters long and contain at least one lowercase letter, one uppercase letter, one number and one special character."
