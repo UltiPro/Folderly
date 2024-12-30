92aa29cb86af7c5403501ff006be7a7581247585
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-12-30 21:56:16 +0100

    Update folder.py

--- Folderly/Backend/models/folder.py
+++ Folderly/Backend/models/folder.py
@@ -5,7 +5,7 @@ class FolderModel(db.Model):
     __tablename__ = "folders"
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, unique=True, nullable=False)
+    path = db.Column(db.String, unique=True, nullable=False)
     users = db.relationship(
         "UserModel", back_populates="folders", secondary="usersfolders"
     )
